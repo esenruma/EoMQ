74375e60fc7f6a3f1d16e45cfc629ad68b234a43
Roma <roma@roma.local>
2016-04-29 12:09:35 +0100

    SubmitButton Fixed - PopUp Alert if no Selections

--- EoMQ/QuestionsViewController.swift
+++ EoMQ/QuestionsViewController.swift
@@ -23,6 +23,7 @@ class QuestionsViewController: UIViewController {
     @IBOutlet var optionB: UIButton!
     @IBOutlet var optionC: UIButton!
     @IBOutlet var optionD: UIButton!
+    @IBOutlet var submit: UIButton!
     
     @IBOutlet var resultsLabel: UILabel!
   
@@ -31,10 +32,10 @@ class QuestionsViewController: UIViewController {
     var rightAnswerFromFetch = String() // store String Correct Answer from Fetch-To compare
     var selectedAnswer = String() // which Option=Selected? // last one selected B4 'Submit'
     
+    var ifOptionSelected : Int = 0 // fix SubmitButton- Ensure Alert if Option Not selected
     
 // ---------------------------------------
     @IBAction func cancelButtonQ(sender: AnyObject) {
-        
         totalCorrects = 0
         totalQuestions = 0
         self.dismissViewControllerAnimated(true, completion: nil)
@@ -61,9 +62,13 @@ class QuestionsViewController: UIViewController {
         self.optionB.enabled = true
         self.optionC.enabled = true
         self.optionD.enabled = true
+        self.submit.enabled = true
         
         // results Label Clear
         self.resultsLabel.text = ""
+        
+        // Reset ifOptionSelected - back to "0"
+        self.ifOptionSelected = 0
         
         // Call RN Q.'s again
         getRandomQuestion()
@@ -72,30 +77,56 @@ class QuestionsViewController: UIViewController {
 // ---------------------------------------
     @IBAction func submitButton(sender: AnyObject) {
         
-        // disable any further options being chosen
-        self.optionA.enabled = false
-        self.optionB.enabled = false
-        self.optionC.enabled = false
-        self.optionD.enabled = false
-        
-        // Check if answer = Correct
-        if self.selectedAnswer == self.rightAnswerFromFetch {
-            self.resultsLabel.text = "That's RIGHT !!!"
-            totalCorrects = totalCorrects + 1
-            
-        } else {
-            self.resultsLabel.text = "Sorry - that's wrong... the right answer is... \(self.rightAnswerFromFetch)"
+        // check if an Option is selected 1st - if Not - goto Alert
+        if self.optionA.backgroundColor == UIColor.yellowColor() {
+            self.ifOptionSelected = 1
+        } else if self.optionB.backgroundColor == UIColor.yellowColor() {
+            self.ifOptionSelected = 1
+        } else if self.optionC.backgroundColor == UIColor.yellowColor() {
+            self.ifOptionSelected = 1
+        } else if self.optionD.backgroundColor == UIColor.yellowColor() {
+            self.ifOptionSelected = 1
         }
         
-        // Highlight next-STEP toDo
-        self.next.backgroundColor = UIColor.redColor()
+        // Alert -reminder if no Options are Selected
+        if self.ifOptionSelected == 0  {
         
-        // Add tot.Q's to GLobal + (ScoreLabel)
-        totalQuestions = totalQuestions + 1
+            let alert = UIAlertController(title: "Alert", message: "Please select an Option!", preferredStyle: .Alert)
+            let cancelAction = UIAlertAction(title: "ok", style: UIAlertActionStyle.Cancel, handler: {
+                UIAlertAction in
+                alert.dismissViewControllerAnimated(true, completion: nil)
+            })
+            alert.addAction(cancelAction)
+            self.presentViewController(alert, animated: true, completion: nil)
+            
+        } else {
         
-        self.scoreLabel.text = "\(totalCorrects) correct out of \(totalQuestions)"
-        print(totalCorrects) ; print(totalQuestions)
+            // disable any further options being chosen
+            self.optionA.enabled = false
+            self.optionB.enabled = false
+            self.optionC.enabled = false
+            self.optionD.enabled = false
+            self.submit.enabled = false
         
+            // Check if answer = Correct
+            if self.selectedAnswer == self.rightAnswerFromFetch {
+                self.resultsLabel.text = "That's RIGHT !!!"
+                totalCorrects = totalCorrects + 1
+            
+            } else {
+                self.resultsLabel.text = "Sorry - that's wrong! It is... \(self.rightAnswerFromFetch)"
+            } // end IF
+        
+            // Highlight next-STEP toDo
+            self.next.backgroundColor = UIColor.redColor()
+        
+            // Add tot.Q's to GLobal + (ScoreLabel)
+            totalQuestions = totalQuestions + 1
+        
+            self.scoreLabel.text = "\(totalCorrects) correct out of \(totalQuestions)"
+            print(totalCorrects) ; print(totalQuestions)
+            
+        } // end 1st IF
     }
     
 // --------------Option Selections-----------------
